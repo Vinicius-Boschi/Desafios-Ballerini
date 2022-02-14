d17c73c1e3960e2725b7737b8e48323d5126bc2f
Vinicius Boschi <vinicius.yg@gmail.com>
2022-02-14 19:32:09 -0300

    Adicionando página inicial e colocando outros projetos nela

---
+++ paginaInicial/script.js
@@ -0,0 +1,30 @@
+let projects = [
+
+    {
+        image: "../paginaInicial/img/troco.jpg",
+        name: "Cálculo de Troco",
+        link: "../troco/index.html"
+    },
+
+    {
+        image: "../paginaInicial/img/desafioExtra.jpg",
+        name: "Desafio Extra",
+        link: "../desafioExtra/index.html"
+    },
+
+    {
+        image: "https://portfolioviniciusboschi.netlify.app/images/ballebot.jpg",
+        name: "Landing Page",
+        link: "https://ballebot.netlify.app/"
+    }
+]
+
+let res = document.querySelector(".res")
+
+function showProjects() {
+    for (let i = 0; i < projects.length; i++){
+        res.innerHTML += "<div class='projects'><a href='" + projects[i].link + "'target=_blank'" +"'>" + "<img class='image' src='" + projects[i].image + "']>" + "<span class='text'>" + projects[i].name + "</span></a></div>"
+    }
+}
+
+showProjects()
